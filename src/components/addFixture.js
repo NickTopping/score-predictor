72b3a7e86ccfa26e44dea13484afdb0568d764bb
NickTopping <nick_97@hotmail.co.uk>
2020-09-06 18:19:23 +0100

    Began work on generating/updating fixtures.

--- src/components/addFixture.js
+++ src/components/addFixture.js
@@ -1,79 +1,89 @@
-import React from "react"
+import React, { useState, useEffect } from "react"; //useState is a hook, manages state in a functional component
 import addFixtureStyles from './addFixture.module.scss'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
 import { DropdownButton, MenuItem } from 'react-bootstrap'
 import Dropdown from 'react-bootstrap/Dropdown'
 import 'bootstrap/dist/css/bootstrap.css';
-import allTeams from '../responses/teams.json';
+    
+async function getFixtures() { //this should be placed in api as /updateGameweek
+    
+    const fixtureArray = await fetch("http://localhost:9000/getAllFixtures")
+        .then(response => response.json());
+    
+    console.log("This won't run until the await has finished");
+    console.log(fixtureArray);
 
-//flag in array to say fixture has been confirmed or score confirmed?
-var teamList = allTeams;
-var arr = []; 
-
-function generateFixtures(teamList) {
-  for (var firstTeam = 0; firstTeam < teamList["teams"].length; firstTeam++) {
-    for (var secondTeam = firstTeam + 1; secondTeam < teamList["teams"].length; secondTeam++) {
-        
-        arr.push( 
-            {
-             date:"2000-01-01",
-             homeTeamId: teamList["teams"][firstTeam].teamId,
-             homeTeamName: teamList["teams"][firstTeam].teamName,
-             awayTeamId: teamList["teams"][secondTeam].teamId,
-             awayTeamName: teamList["teams"][secondTeam].teamName,
-             homeGoals: 0, 
-             awayGoals: 0,        
-            });
-    }
-  }
-  for (var firstTeam = 0; firstTeam < teamList["teams"].length; firstTeam++) {
-    for (var secondTeam = firstTeam + 1; secondTeam < teamList["teams"].length; secondTeam++) {
-        
-        arr.push( 
-            {
-            date:"2000-01-01",
-            homeTeamId: teamList["teams"][secondTeam].teamId,
-            homeTeamName: teamList["teams"][secondTeam].teamName,
-            awayTeamId: teamList["teams"][firstTeam].teamId,
-            awayTeamName: teamList["teams"][firstTeam].teamName,
-            homeGoals: 0, 
-            awayGoals: 0,         
-            });
-    }
-  }
-  console.log(arr);
-
-    /*const handleSaveToPC = arr => {
-    const fileData = JSON.stringify(arr);
-    const blob = new Blob([fileData], {type: "text/plain"});
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.download = 'testFile.json';
-    link.href = url;
-    link.click();
-  }
-
-  handleSaveToPC(arr);*/
+    //use fixtureArray to show all fixtures per gw
 }
 
-const AddFixture = () => {
-    return (<div>teams</div>
-    )}
+async function updateGameweek() { //this should be placed in api as /updateGameweek (need to call updateGameweek api in here)
+    
+    //get json file in order to edit it
+    const fixtureArray = await fetch("http://localhost:9000/getAllFixtures")
+        .then(response => response.json());
+    
+    console.log("This won't run until the await has finished");
+    console.log(fixtureArray);
 
-    export default AddFixture
+    let gameWeekID = 30; //pass gameWeek as args
+    let fixtureToMoveIndex = fixtureArray[0].rounds[0].matches.findIndex(e => e.fixtureId === "EVELIV28373"); //pass fixtureId as args
 
-generateFixtures(teamList);
+    console.log("Index of fixture to be moved:");
+    console.log(fixtureToMoveIndex);
 
+    let fixture = fixtureArray[0].rounds[0].matches[fixtureToMoveIndex]
 
-/*const gwOptions = [];
+    fixtureArray[0].rounds[0].matches.splice(fixtureToMoveIndex, 1);
+    console.log("Fixture:");
+    console.log(fixture);
 
-for(let i=1; i<=38; i++)
-{
-    gwOptions.push({key: i, text: i, value: i});
-} 
+    let gameWeekIndex = fixtureArray[0].rounds.findIndex(e => e.gw === gameWeekID);
+    console.log("GW index:");
+    console.log(gameWeekIndex);
+
+    fixtureArray[0].rounds[gameWeekIndex].matches.push(fixture);
+    console.log("GW index 2:");
+    console.log(fixtureArray[0].rounds[gameWeekIndex]);
+    
+    console.log("Array:");
+    console.log(fixtureArray);
+}
+
+export default function AddFixture() {
+
+    const [fixtures, setFixtures] = useState({
+        name: '',
+        rounds: [],
+    });
+
+    const url = "http://localhost:9000/generateFixtures";
+
+    useEffect(() => { //mounts
+        fetch(url)
+            .then(response => response.json())
+            .then(data => setFixtures(data && data[0] || fixtures));
+    }, []);
+
+    return (
+        <div className={addFixtureStyles.wrapper}>
+            <div>teams</div>
+            <div className={addFixtureStyles.btnSubmit}>
+                <Button id='btnGenerateFixtureList' variant="primary" onClick={() => alert("Need to generate fixtures on button click, not on page load.")}>Generate Fixtures</Button> 
+                <div>
+                    <span>Round Name: {fixtures.name}</span>
+                    <span>Round Length: {fixtures.rounds.length}</span>
+                </div>
+                
+            </div>  
+            <div className={addFixtureStyles.btnSubmit}>
+                <Button id='btnUpdateGameweek' variant="primary" onClick={() => updateGameweek()}>Update Gameweek</Button>
+            </div> 
+        </div>    
+)}
+
 //Change home/away teams to be dropdown box or type (select from existing teams)
-const AddFixture = () => {
+/*const AddFixture = () => {
     return (
         <div className={addFixtureStyles.wrapper}>
             <div className={addFixtureStyles.controlSpacing}>
